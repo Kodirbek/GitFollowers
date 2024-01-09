0722dfd30b91e82e5a956203c48ac7616dc70bd9
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-01-09 23:52:04 +0900

    fix: add container view constraints

--- GitFollowers/GFAlertVC.swift
+++ GitFollowers/GFAlertVC.swift
@@ -41,6 +41,7 @@ class GFAlertVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContainerView()
     }
     
     
@@ -48,9 +49,18 @@ class GFAlertVC: UIViewController {
     
     func configureContainerView() {
         self.view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        
+        NSLayoutConstraint.activate([
+            containerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
     }
     
 
