6240859e2bc4ebb6ccb56edcf04c46ad9e648eaf
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-02-20 07:33:07 +0900

    feat: configure FavoriteVC

--- GitFollowers/Screens/FavoritesListVC.swift
+++ GitFollowers/Screens/FavoritesListVC.swift
@@ -9,19 +9,53 @@ import UIKit
 
 class FavoritesListVC: UIViewController {
 
+    // MARK: - Properties
+    private let tableView: UITableView = UITableView()
+    private var favorites: [Follower] = []
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
+        configureViewController()
+        getFavorites()
         
-        PersistenceManager.retrieveFavorites { result in
+    }
+    
+    
+    // MARK: - Methods
+    private func configureViewController() {
+        self.view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
             switch result {
                 case .success(let favorites):
-                    print(favorites)
+                    self?.favorites = favorites
                 case .failure(let error):
                     print(error)
             }
         }
     }
-    
+}
 
+
+// MARK: - UITableView delegate methods
+extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
